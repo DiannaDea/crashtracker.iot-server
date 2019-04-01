b2047ded10cc90d914b149af3f6a3e531e9f099e
Diana Baburina <diana.baburina@code-care.pro>
2019-04-01 21:53:44 +0300

    add cron for temperature parsing

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const Koa = require('koa');
+const cron = require('node-cron');
 
 const app = new Koa();
+const logger = require('./utils/logger');
 const { parseTemperature } = require('./services/temperatureParser');
 
 require('./services/mongoConnection');
 
-parseTemperature();
+cron.schedule('*/5 * * * *', () => {
+  logger.info(`Parse temperature ${new Date()}`);
+  parseTemperature();
+});
 
 module.exports = app;
